08a0421ec0ed62de761a0e4e7429684dd01a6af3
masterful-array-3850 <crgammage@gmail.com>
2020-04-09 19:49:38 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,2 +1,5 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 superheroines.push("wonder woman");
+
+var citis = ["New York", "San Francisco"]
+cities.unshift("Philadelphia")
