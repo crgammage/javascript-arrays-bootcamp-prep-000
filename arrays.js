95d8591b4119c2405cc1f6f45561cf5ff4c1006f
masterful-array-3850 <crgammage@gmail.com>
2020-04-09 19:57:49 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,1 @@
-var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
-superheroines.push("wonder woman");
-
-var citis = ["New York", "San Francisco"]
-cities = ["Philadelphia", ...cities]
-const cats = ["Milo", "Garfield"]
-const moreCats = ["Felix", ...cats]
+function addElementToBeginningOfArray(array, element)
