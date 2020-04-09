b764857c13d7ed5203e1acc43d6933141dcb1b6c
masterful-array-3850 <crgammage@gmail.com>
2020-04-09 20:22:55 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -2,7 +2,7 @@ var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 var candyString = 'foo'
 
 function addElementToBeginningOfArray(chocolateBars, candyString) {
-  return [candyString, ...chocolateBars]
+  return [chocolateBars]
 }
 
 function destructivelyAddElementToBeginningOfArray(chocolateBars, element) {
