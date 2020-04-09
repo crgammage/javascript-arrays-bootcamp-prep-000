3ce0b47be1880c74e7bc8a6245137f15b6d40c56
masterful-array-3850 <crgammage@gmail.com>
2020-04-09 20:08:35 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,6 +1,6 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles']
 
-function addElementToBeginningOfArray(chocolateBars, element) {
-  chocolateBars.unshift(element);
+function addElementToBeginningOfArray(chocolateBars, 'foo') {
+  chocolateBars.unshift('foo');
   return chocolateBars
 }
